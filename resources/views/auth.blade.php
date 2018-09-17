ebdfdf42397d8132610b92fc41eefe7a11753bcd
alevelSoroka <alka8@mail.ru>
2018-09-17 21:27:56 +0300

    added 3 buttons on first page TEST version

---
+++ resources/views/auth.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Студенты A-Level</title>
+ </head>
+<body>
+ <div class = "container">
+
+  <div class = "row">
+   <div class = "col-md-3 col-sm-6">
+    <button>  <a href="{{route('started')}}">Студенты</a></button>
+   </div>
+  </div>
+
+  <div class = "row">
+   <div class = "col-md-1 col-sm-6">
+    <button>  <a href="{{route('started')}}">Преподаватели</a></button>
+   </div>
+  </div>
+
+  <div class = "row">
+   <div class = "col-md-1 col-sm-6">
+    <button>  <a href="{{route('started')}}">Партнеры</a></button>
+   </div>
+  </div>
+
+ </div>
+
+
+
+
+ </body>
+</html>
