588b216f289a6457d9b438804646554286f9b5a3
Skrypnik Andrii <skrypnik.andrii@gmail.com>
2018-09-27 18:17:50 +0300

    add model filable

--- database/migrations/2018_09_18_133547_create_contact_person_entity_table.php
+++ database/migrations/2018_09_18_133547_create_contact_person_entity_table.php
@@ -17,7 +17,7 @@ class CreateContactPersonEntityTable extends Migration
         Schema::create('contact_persons', function (Blueprint $table) {
             $table->increments('id');
             $table->text('comment')->nullable();
-            $table->integer('person_id')->nullable()->unsigned();
+            $table->integer('person_id')->unsigned();
             $table->integer('position_id')->nullable()->unsigned();
             $table->integer('direction_id')->nullable()->unsigned();
             $table->integer('company_id')->nullable()->unsigned();
