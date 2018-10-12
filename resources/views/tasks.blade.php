d2b2fd6b83e2d04f54e0b015dd4115a4c4db0476
EvgeniyaSt <stukashova.evgeniya@gmail.ru>
2018-10-13 01:27:05 +0300

    Merge https://github.com/alaster1985/project_crm
    # Conflicts:
    #	app/Http/Controllers/StudentsController.php
    #	resources/views/students.blade.php
    #	resources/views/tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,31 +1,89 @@
 @extends('layouts.nav')
 @section('title', 'Задачи')
 <div>
-
-    <ul>
+    <h4> Список заданий </h4>
+    <table id="tableTasks" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+        <i class="fa fa-sort float-right" aria-hidden="true"></i>
+        </th>
+        <th class="th-sm">Задача
+            <i class="fa fa-sort float-right" aria-hidden="true"></i>
+        </th>
+        <th class="th-sm">Описание
+            <i class="fa fa-sort float-right" aria-hidden="true"></i>
+        </th>
+        <th class="th-sm">Дедлайн
+            <i class="fa fa-sort float-right" aria-hidden="true"></i>
+        </th>
+        <th class="th-sm">Заказчик
+            <i class="fa fa-sort float-right" aria-hidden="true"></i>
+        </th>
+        <th class="th-sm">Исполнитель
+            <i class="fa fa-sort float-right" aria-hidden="true"></i>
+        </th>
+        <th class="th-sm">Отчет
+            <i class="fa fa-sort float-right" aria-hidden="true"></i>
+        </th>
+        <th class="th-sm">Выполнено
+            <i class="fa fa-sort float-right" aria-hidden="true"></i>
+        </th>
+        </tr>
+        </thead>
+        <tbody>
         @if ($all_tasks)
             @foreach ($all_tasks as $index)
-                <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->task_name}}</a>
-                <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->description}}</a>
-                <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->dead_line}}</a>
-                <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->customerName}}</a>
-                <br>
-            @endforeach
-            {{ $all_tasks->links() }}
-        @endif
+                <tr>
+                    <td>
+                        <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->task_name}}</a>
+                    </td>
+                    <td>
+                        <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->description}}</a>
+                    </td>
+                    <td>
+                        <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->dead_line}}</a>
+                    </td>
+                    <td>
+                        <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->customerName}}</a>
+                    </td>
+                    @endforeach
+                    {{ $all_tasks->links() }}
+                    @endif
+                    @foreach ($doer as $index)
+                        <td>
+                            <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->name}}</a>
+                        </td>
+                    @endforeach
+                    @foreach($all_tasks as $index)
+                        <td>
+                            <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->doers_report}}</a>
+                        </td>
+                        <td>
+                            <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->task_completed}}</a>
+                        </td>
+                    @endforeach
+                </tr>
 
-        @foreach ($doer as $index)
-            <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->name}}</a>
-            <br>
-        @endforeach
-        @foreach($all_tasks as $index)
-            <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->doers_report}}</a>
-            <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->task_completed}}</a>
-            <br>
-        @endforeach
+    {{--  <ul>--}}
+    {{-- @if ($all_tasks)
+         @foreach ($all_tasks as $index)
+             <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->task_name}}</a>
+             <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->description}}</a>
+             <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->dead_line}}</a>
+             <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->customerName}}</a>
+             <br>
+         @endforeach
+         {{ $all_tasks->links() }}
+     @endif--}}
 
-
-    </ul>
+    {{--   @foreach ($doer as $index)
+           <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->name}}</a>
+           <br>
+       @endforeach
+       @foreach($all_tasks as $index)
+           <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->doers_report}}</a>
+           <a href="{{route('tasks.view', ['id' => $index->id] )}}">{{$index->task_completed}}</a>
+           <br>
+       @endforeach
+   </ul>--}}
 </div>
 
 
