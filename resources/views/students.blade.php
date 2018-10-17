769c48fe408620c1a4c91fe76d3caef611683766
EvgeniyaSt <stukashova.evgeniya@gmail.ru>
2018-10-17 20:53:27 +0300

    Merge https://github.com/alaster1985/project_crm
    # Conflicts:
    #	app/Http/Controllers/StudentsController.php
    #	resources/views/students.blade.php
    #	resources/views/tasks.blade.php

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -111,70 +111,6 @@
     </div>
 
     <div class="col-md-2 col-sm-6 ">
-        {{--     <!-- form adding students to DB.
-             Use data in php by
-             $request->session()->keep(['name', 'email', 'site', 'text_area']);
-             -->
-             <div class="container">
-                 <div>
-                     <img style="max-width:100px; margin-top: -7px;"
-                          src="/images/group.png">
-                 </div>
-                 <!-- Trigger the modal with a button -->
-                 <button type="submit" class="btn btn-default" data-toggle="modal" data-target="#myModal">Добавить нового
-                     студента
-                 </button>
-
-                 <!-- Modal -->
-                 <div class="modal fade" id="myModal" role="dialog">
-                     <div class="modal-dialog">
-
-                         <!-- Modal content-->
-                         <div class="modal-content">
-                             <div class="modal-header">
-                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                 <h4 class="modal-title">Добавление нового студента</h4>
-                             </div>
-                             <div class="modal-body">
-                                 <form action="{{Route('add.student')}}" method="post" enctype="multipart/form-data">
-                                     @csrf
-                                     <p>Введите данные</p>
-                                     <p>Имя студента</p>
-                                     <p><input name="student_name"></p>
-                                     <p>Фамалия студента</p>
-                                     <p><input name="student_surname"></p>
-                                     <p>Дата рождения студента</p>
-                                     <p><input name="student_birth"></p>
-                                     <p>Адрес студента</p>
-                                     <p><input name="student_adress"></p>
-                                     <p>E-mail студента</p>
-                                     <p><input name="student_mail"></p>
-                                     <p>Телефон студента</p>
-                                     <p><input name="student_phone"></p>
-                                     </br>
-                                     <input type="submit" value="Add new student">
-                                 </form>
-                             </div>
-                             <div class="modal-footer">
-                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-                 <form action="{{Route('add.student')}}">
-                     <input type="submit" value="Add new student">
-                 </form>
-             </div>
-             <form role="search">
-                 <div class="form-group">
-
-                     <input type="search" name="search" class="form-control" id="search" placeholder="Поиск по сайту">
-                     <div id="findResult">
-                     </div>
-
-                 </div>
-                 <button type="submit" class="btn btn-default">Поиск</button>
-             </form>--}}
 
         <div class="panel-group" id="accordion">
             <div class="panel panel-default">
@@ -212,6 +148,9 @@
                 </div>
             </div>
         </div>
+
+
+        <button type="button" onclick="window.location='{{ route("addstud")}}'">Добавить нового студента</button>
     </div>
 
 @extends('layouts.footer')
