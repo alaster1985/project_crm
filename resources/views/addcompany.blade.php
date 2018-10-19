5d23e9ddc11f05a462751fbaac8b5c86d84b632d
EvgeniyaSt <stukashova.evgeniya@gmail.ru>
2018-10-20 00:47:51 +0300

    Merge https://github.com/alaster1985/project_crm
    # Conflicts:
    #	resources/views/addstudent.blade.php
    #	resources/views/students.blade.php

--- resources/views/addcompany.blade.php
+++ resources/views/addcompany.blade.php
@@ -1,39 +1,87 @@
 @extends('layouts.nav')
 @section('title', 'Add Company')
 
-<form action="{{Route('add.company')}}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <p>Добавление новой компании</p>
-    <p>Название компании</p>
-    <p><input name="company_name"></p>
-    <p>Адрес офиса компании</p>
-    <p><input name="company_address"></p>
-    <p>Сайт</p>
-    <p><input name="site"></p>
-    <p>Статус взаимодействия</p>
-    <select name="status">
-        <option value="Партнеры">Партнеры</option>
-        <option value="Ведется диалог">Ведется диалог</option>
-        <option value="Потенциальные">Потенциальные</option>
-        <option value="Неотслеживаемые">Неотслеживаемые</option>
-    </select>
-    <p>Тип сотрудничества</p>
-    <select name="type">
-        <option value="Трудоустройство">Трудоустройство</option>
-        <option value="Информационное">Информационное</option>
-        <option value="партнерство">партнерство</option>
-        <option value="Проведение мероприятий">Проведение мероприятий</option>
-        <option value="Отсутствует">Отсутствует</option>
-    </select>
-    <p>Стэк технологий</p>
-    <select id="stacks" name="stack_id"></select>
-    <p>Комментарий к стэк-технологии</p>
-    <p><input name="stack_comment"></p>
-    <p>Логотип</p>
-    <p><input type="file" name="file" id="fileToUpload">
-    <p>Комментарий</p>
-    <p><input name="company_comment"></p>
-    <script src="/js/run2.js"></script>
-    <input type="submit" value="Add new company">
-</form>
+<div class="container-fluid">
+    <form action="{{Route('add.company')}}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <p>Добавление новой компании</p>
+
+        <div class="row">
+            <div class="form-group col-md-3 col-sm-2">
+                <label for="person_name">Название компании</label>
+                <input class="form-control" name="company_name" placeholder="Название компании">
+                <p class="help-block">*обязательное поле</p>
+            </div>
+
+            <div class="form-group col-md-3 col-sm-2">
+                <label for="person_name">Адрес офиса компании</label>
+                <input class="form-control" name="company_address" placeholder="Адрес офиса компании">
+                <p class="help-block">*обязательное поле</p>
+            </div>
+
+
+            <div class="form-group col-md-3 col-sm-2">
+                <label for="person_name">Сайт</label>
+                <input class="form-control" name="site" placeholder="Сайт">
+                <p class="help-block">*обязательное поле</p>
+            </div>
+
+            <div class="form-group col-md-2 col-sm-2">
+                <label for="file">Логотип</label>
+                <input type="file" name="file" id="fileToUpload">
+                <p class="help-block">*не обязательное поле</p>
+            </div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="form-group col-md-3 col-sm-2">
+                <label for="company_comment">Комментарий</label>
+                <input class="form-control" name="company_comment">
+                <p class="help-block">*не обязательное поле</p>
+            </div>
+
+
+            <div class="form-group col-md-3 col-sm-2">
+                <label for="status">Статус взаимодействия</label>
+                <div>
+                    <select class="form-control" name="status">
+                        <option value="Партнеры">Партнеры</option>
+                        <option value="Ведется диалог">Ведется диалог</option>
+                        <option value="Потенциальные">Потенциальные</option>
+                        <option value="Неотслеживаемые">Неотслеживаемые</option>
+                    </select>
+                </div>
+                <p class="help-block">*обязательное поле</p>
+            </div>
+
+
+
+            <div class="form-group col-md-3 col-sm-2">
+                <label for="type">Тип сотрудничества</label>
+                <div>
+                    <select class="form-control" name="type">
+                        <option value="Трудоустройство">Трудоустройство</option>
+                        <option value="Информационное">Информационное</option>
+                        <option value="партнерство">партнерство</option>
+                        <option value="Проведение мероприятий">Проведение мероприятий</option>
+                        <option value="Отсутствует">Отсутствует</option>
+                    </select>
+                </div>
+                <p class="help-block">*обязательное поле</p>
+            </div>
+            <div class="form-group col-md-2 col-sm-2">
+                <label for="stacks">Стэк технологий</label>
+                <input class="form-control" name="stacks" placeholder="Стэк технологий">
+                <p>Комментарий к стэк-технологии</p>
+                <p><input class="form-control" name="stack_comment"></p>
+                <p class="help-block">*не обязательное поле</p>
+            </div>
+        </div>
+        <script src="/js/run2.js"></script>
+        <div><input type="submit" value="Add new company"></div>
+
+    </form>
+</div>
 @extends('layouts.footer')
--- resources/views/addcontactperson.blade.php
+++ resources/views/addcontactperson.blade.php
@@ -1,29 +1,62 @@
 @extends('layouts.nav')
 @section('title', 'Add Contact Person')
+<div class="container-fluid">
+    <form action="{{Route('add.contactperson')}}" method="post" enctype="multipart/form-data">
+        @csrf
+        <p>Добавление нового контактного лица</p>
+        <div class="row">
 
-<form action="{{Route('add.contactperson')}}" method="post" enctype="multipart/form-data">
-    @csrf
-    <p>Добавление нового контактного лица</p>
-    <p>ФИО контактного лица</p>
-    <p><input name="person_name"></p>
-    <p>Адрес контактного лица</p>
-    <p><input name="person_address"></p>
-    <p>Должность контактного лица</p>
-    <select id="position" name="position_id"></select>
-    <p>Направление в IT</p>
-    <select id="direction" name="direction_id"></select>
-    <p>IT компания в которой работает</p>
-    <select id="companies" name="company_id"></select>
-    <p>Комментарий</p>
-    <p><input name="contact_person_comment"></p>
-    <p>Инструмент связи</p>
-    <p><input name="communication_tool"></p>
-    <p>Контакт</p>
-    <p><input name="contact"></p>
-    <p>Коментарий</p>
-    <p><input name="contact_comment"></p>
-    </br>
-    <input type="submit" value="Add new contact person">
-    <script src="/js/run2.js"></script>
-</form>
+            <div class="form-group col-md-3 col-sm-2">
+                <label for="person_name">ФИО контактного лица</label>
+                <input class="form-control" name="person_name" placeholder="ФИО контактного лица">
+                <p class="help-block">*обязательное поле</p>
+            </div>
+
+            <div class="form-group  col-md-3 col-sm-2">
+                <label for="person_address">Адрес контактного лица</label>
+                <input class="form-control" name="person_address" placeholder="Адрес контактного лица">
+                <p class="help-block">*обязательное поле</p>
+            </div>
+            <div class="form-group col-md-3 col-sm-2">
+                <label for="contact_person_comment">Комментарий</label>
+                <input class="form-control" name="contact_person_comment">
+                <p class="help-block">*не обязательное поле</p>
+            </div>
+
+            <div class="form-group col-md-2 col-sm-2">
+                <label for="communication_tool">Инструмент связи</label>
+                <p><input class="form-control" name="communication_tool"></p>
+                <p>Контакт</p>
+                <p><input class="form-control" name="contact"></p>
+                <p>Коментарий</p>
+                <p><input class="form-control" name="contact_comment"></p>
+                <p class="help-block">*обязательное поле</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-group  col-md-3 col-sm-3">
+                <label for="direction">IT компания в которой работает</label>
+                <select class="form-control" id="companies" name="company_id"></select>
+                <p class="help-block">*не обязательное поле</p>
+            </div>
+            <div class="form-group  col-md-3 col-sm-3">
+                <label for="position">Должность контактного лица</label>
+                <select class="form-control" id="position" name="position_id"></select>
+                <p class="help-block">*не обязательное поле</p>
+            </div>
+
+            <div class="form-group  col-md-3 col-sm-2">
+                <label for="direction">Направление в IT</label>
+                <select class="form-control" id="direction" name="direction_id"></select>
+                <p class="help-block">*не обязательное поле</p>
+            </div>
+
+
+        </div>
+
+
+        <div><input type="submit" value="Add new contact person"></div>
+        <script src="/js/run2.js"></script>
+    </form>
+</div>
 @extends('layouts.footer')
