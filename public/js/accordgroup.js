0d0c68077874744fee4919a13686015080454386
EvgeniyaSt <stukashova.evgeniya@gmail.ru>
2018-10-23 00:52:23 +0300

    Merge https://github.com/alaster1985/project_crm
    # Conflicts:
    #	resources/views/addstudent.blade.php
    #	resources/views/students.blade.php

--- public/js/accordgroup.js
+++ public/js/accordgroup.js
@@ -13,30 +13,30 @@ function aaa(item, id) {
         for (var i = document.getElementById('myTable').getElementsByTagName('tr').length -1; i; i--) {
             document.getElementById('myTable').deleteRow(i);
         }
-            console.log( location.origin)
+        console.log( location.origin)
         jsonPost(location.origin+"/employees/studentsdirection", id)
             .then(response => studget(JSON.parse(response)))
             .then(studget(someth))
-            function studget(studdata) {
+        function studget(studdata) {
             // document.getElementById('stres').innerHTML = '';
-//            console.log(studdata)
-                var rowTable =0;
-                for (var gr = 0; gr < studdata.length; gr++) {
-                    if (filter == 7) {
+//           console.log(studdata)
+            var rowTable =0;
+            for (var gr = 0; gr < studdata.length; gr++) {
+                if (filter == 7) {
 
-                        console.log(filter)
-                        if (studdata[gr]['name'] == 'fox') {
-                            console.log(studdata[gr]['name'])
-                            var row = mytable.insertRow(rowTable);
-                            var cell1 = row.insertCell(0);
-                            var cell2 = row.insertCell(1);
-                            cell1.innerHTML = studdata[gr]['name'];
-                            cell2.innerHTML = gr;
-                            console.log(mytable)
-                            rowTable ++;
-                        }
+                    console.log(filter)
+                    if (studdata[gr]['name'] == 'fox') {
+                        console.log(studdata[gr]['name'])
+                        var row = mytable.insertRow(rowTable);
+                        var cell1 = row.insertCell(0);
+                        var cell2 = row.insertCell(1);
+                        cell1.innerHTML = studdata[gr]['name'];
+                        cell2.innerHTML = gr;
+                        console.log(mytable)
+                        rowTable ++;
                     }
                 }
+            }
 
             // for (var gr = 0; gr < studdata.length; gr++) {
             //     var a = document.createElement('a');
