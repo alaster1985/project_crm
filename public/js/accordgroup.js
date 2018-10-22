5bd8997b8646c425a7a698724be7f2fdfb77f076
kib06041960@gnail.com <kib06041960@gnail.com>
2018-10-22 13:07:11 +0300

    js table added

--- public/js/accordgroup.js
+++ public/js/accordgroup.js
@@ -4,47 +4,43 @@ var ll
 var filter = 7;
 
 var ind = document.querySelectorAll(".direct");
+
+
 ind.forEach(aaa)
+
+
 function aaa(item, id) {
     item.onclick = function(){
-        // document.getElementById('stres').innerHTML = '';
 
-        // CLEARE TABLE BY DELETE ROWS
+        // CLEAR TABLE BY DELETE ROWS
         for (var i = document.getElementById('myTable').getElementsByTagName('tr').length -1; i; i--) {
             document.getElementById('myTable').deleteRow(i);
         }
-            console.log( location.origin)
+
         jsonPost(location.origin+"/employees/studentsdirection", id)
             .then(response => studget(JSON.parse(response)))
             .then(studget(someth))
             function studget(studdata) {
-            // document.getElementById('stres').innerHTML = '';
-//            console.log(studdata)
                 var rowTable =0;
                 for (var gr = 0; gr < studdata.length; gr++) {
                     if (filter == 7) {
 
                         console.log(filter)
+                        console.log(studdata[2])
                         if (studdata[gr]['name'] == 'fox') {
                             console.log(studdata[gr]['name'])
                             var row = mytable.insertRow(rowTable);
                             var cell1 = row.insertCell(0);
                             var cell2 = row.insertCell(1);
+                            var cell3 = row.insertCell(1);
                             cell1.innerHTML = studdata[gr]['name'];
                             cell2.innerHTML = gr;
+                            cell3.innerHTML =studdata[gr]['direction'];
                             console.log(mytable)
                             rowTable ++;
                         }
                     }
                 }
-
-            // for (var gr = 0; gr < studdata.length; gr++) {
-            //     var a = document.createElement('a');
-            //     a.setAttribute('href', "students/show/"+ studdata[gr]['person_id']);
-            //     a.innerHTML = studdata[gr]['name']
-            //     studext.appendChild(a)
-            //     studext.appendChild(document.createElement('br'))
-            // }
         }
     }
 }
@@ -68,3 +64,10 @@ function navfun(itemgr, idgr) {
         }
     }
 }
+// for (var gr = 0; gr < studdata.length; gr++) {
+//     var a = document.createElement('a');
+//     a.setAttribute('href', "students/show/"+ studdata[gr]['person_id']);
+//     a.innerHTML = studdata[gr]['name']
+//     studext.appendChild(a)
+//     studext.appendChild(document.createElement('br'))
+// }
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="{{ asset('/css/styles.css') }}"/>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>--}}
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     <title>@yield('title')</title>
