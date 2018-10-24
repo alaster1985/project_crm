13ab69cbf4cf1509446c324365187f441a3ed59e
EvgeniyaSt <stukashova.evgeniya@gmail.ru>
2018-10-24 21:30:38 +0300

    Merge https://github.com/alaster1985/project_crm
    # Conflicts:
    #	resources/views/students.blade.php

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -3,14 +3,14 @@
 @csrf
 <meta name="csrf-token" content="{{ csrf_token() }}"/>
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 
-{{--<script type="text/javascript" src="/js/tablesort.js"></script>--}}
-{{--<script type="text/javascript" src="/js/jquery-latest.js"></script>--}}
-{{--<script type="text/javascript" src="/js/jquery.tablesorter.js"></script>--}}
+    {{--<script type="text/javascript" src="/js/tablesort.js"></script>--}}
+    {{--<script type="text/javascript" src="/js/jquery-latest.js"></script>--}}
+    {{--<script type="text/javascript" src="/js/jquery.tablesorter.js"></script>--}}
 
 </head>
 
@@ -32,7 +32,8 @@
 
                             <div class="panel-heading">
                                 <h4 class="panel-title">
-                                    <a data-toggle="collapse" data-parent="#accordionmenu" href="#collapse{{$direction ->id}}">
+                                    <a data-toggle="collapse" data-parent="#accordionmenu"
+                                       href="#collapse{{$direction ->id}}">
                                         <span class="direct" id="{{$direction ->id}}">{{$direction ->direction}}</span>
                                     </a>
                                 </h4>
@@ -42,7 +43,9 @@
                                 <ul class="list-group">
                                     @foreach($groups as $group)
                                         @if($direction->id == $group->direction_id)
-                                            <li class="list-group-item"><a href="#" class="navlink" id="{{$group->group_name}}">{{$group->group_name}}</a></li>
+                                            <li class="list-group-item"><a href="#" class="navlink"
+                                                                           id="{{$group->group_name}}">{{$group->group_name}}</a>
+                                            </li>
                                         @endif
                                     @endforeach
                                 </ul>
@@ -57,9 +60,11 @@
             </div>
         </div>
     </div>
+
     <div id="stres" class="col-md-8 col-sm-6">
         <h4> Список студентов </h4>
-        <table id="myTable" class="table table-striped table-bordered table-sm tablesorter">
+        <table id="myTable" class="table table-striped table-bordered table-sm tablesorter"
+               style="height:500px;display:block;overflow:scroll">
             <thead>
             <tr>
                 <th>First Name</th>
@@ -67,14 +72,11 @@
                 <th>Direction</th>
                 <th>Learning Status</th>
                 <th>Employment Status</th>
-
             </tr>
-                </thead>
+            </thead>
             <tbody>
             </tbody>
         </table>
-
-
     </div>
 
     <div class="col-md-2 col-sm-6 ">
@@ -99,51 +101,24 @@
                 <option value="Other">Other</option>
             </select>
         </div>
-
-       {{-- <div class="panel-group" id="accordion">
-            <div class="panel panel-default">
-                <!-- Заголовок 1 панели -->
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Статус обучения</a>
-                    </h4>
-                </div>
-                <div id="collapseTwo" class="panel-collapse collapse">
-                    <!-- Содержимое 1 панели -->
-                    <div class="panel-body">
-                        @foreach ($learning_status as $status)
-                            <li><a class="dropdown-item" href="#"> {{$status->learning_status}}</a></li>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-            <!-- 2 панель -->
-            <div class="panel panel-default">
-                <!-- Заголовок 2 панели -->
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">Статус
-                            трудоустройства</a>
-                    </h4>
-                </div>
-                <div id="collapseThree" class="panel-collapse collapse">
-                    <!-- Содержимое 2 панели -->
-                    <div class="panel-body">
-                        @foreach ($employment_status as $status)
-                            <li><a class="dropdown-item" href="#"> {{$status->employment_status}}</a></li>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </div>--}}
-        <button type="button" onclick="window.location='{{ route("addstud")}}'">Добавить нового студента</button>
+        <div>
+            <br>
+            <button type="button" onclick="window.location='{{ route("addstud")}}'">Добавить нового студента</button>
+        </div>
+        <div>
+            <br>
+            <button type="button">Отправить СМС</button>
+        </div>
+        <div>
+            <br>
+            <button type="button">Отправить e-mail</button>
+        </div>
     </div>
 </div>
 
 
+@extends('layouts.footer')
 
-    @extends('layouts.footer')
-
-    <script src="/js/run.js"></script>
-    {{--<script src="/js/studentSelectGroupDirection.js"></script>--}}
-    <script src="/js/accordgroup.js"></script>
+<script src="/js/run.js"></script>
+{{--<script src="/js/studentSelectGroupDirection.js"></script>--}}
+<script src="/js/accordgroup.js"></script>
