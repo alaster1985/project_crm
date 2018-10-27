6de2304cce6fb6bc8020d236a487d46031983ce1
EvgeniyaSt <stukashova.evgeniya@gmail.ru>
2018-10-27 19:13:41 +0300

    Merge https://github.com/alaster1985/project_crm
    # Conflicts:
    #	resources/views/students.blade.php

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -13,99 +13,102 @@
     {{--<script type="text/javascript" src="/js/jquery.tablesorter.js"></script>--}}
 
 </head>
+<div class="color_page">
+    <div class="container-fluid ">
+        <div class="col-md-2 col-sm-4 ">
 
-<div class="container-fluid">
-    <div class="col-md-2 col-sm-4 ">
+            <div id="menu">
+                <div class="navbar-header ">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#sideNavbar">
+                        <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="sideNavbar">
 
-        <div id="menu">
-            <div class="navbar-header ">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#sideNavbar">
-                    <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
-                </button>
-            </div>
-            <div class="collapse navbar-collapse" id="sideNavbar">
+                    <div class="panel-group" id="accordionmenu">
 
-                <div class="panel-group" id="accordionmenu">
+                        @forelse($directions as $direction)
+                            <div class="panel panel-default">
 
-                    @forelse($directions as $direction)
-                        <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a data-toggle="collapse" data-parent="#accordionmenu"
+                                           href="#collapse{{$direction ->id}}">
+                                            <span class="direct" id="{{$direction ->id}}">{{$direction ->direction}}</span>
+                                        </a>
+                                    </h4>
+                                </div>
 
-                            <div class="panel-heading">
-                                <h4 class="panel-title">
-                                    <a data-toggle="collapse" data-parent="#accordionmenu"
-                                       href="#collapse{{$direction ->id}}">
-                                        <span class="direct" id="{{$direction ->id}}">{{$direction ->direction}}</span>
-                                    </a>
-                                </h4>
+                                <div id="collapse{{$direction ->id}}" class="panel-collapse collapse">
+                                    <ul class="list-group">
+                                        @foreach($groups as $group)
+                                            @if($direction->id == $group->direction_id)
+                                                <li class="list-group-item"><a href="#" class="navlink"
+                                                                               id="{{$group->group_name}}">{{$group->group_name}}</a>
+                                                </li>
+                                            @endif
+                                        @endforeach
+                                    </ul>
+                                </div>
                             </div>
+                        @empty
+                        @endforelse
 
-                            <div id="collapse{{$direction ->id}}" class="panel-collapse collapse">
-                                <ul class="list-group">
-                                    @foreach($groups as $group)
-                                        @if($direction->id == $group->direction_id)
-                                            <li class="list-group-item"><a href="#" class="navlink"
-                                                                           id="{{$group->group_name}}">{{$group->group_name}}</a>
-                                            </li>
-                                        @endif
-                                    @endforeach
-                                </ul>
-                            </div>
-                        </div>
-                    @empty
-                    @endforelse
+                    </div>
 
                 </div>
-
             </div>
         </div>
-    </div>
-    <div id="stres" class="col-md-8 col-sm-6">
-        <h4 class="name_table"> Список студентов </h4>
-        <table id="myTable" class="table table-striped table-bordered table-sm tablesorter"
-               style="height:500px;display:block;overflow:scroll">
-            <thead>
-            <tr>
-                <th>First Name</th>
-                <th>Group</th>
-                <th>Direction</th>
-                <th>Learning Status</th>
-                <th>Employment Status</th>
-            </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
+        <div id="stres" class="col-md-8 col-sm-6 ">
+            <h4 class="name_table"> Список студентов </h4>
+
+            <table id="myTable" class="table table-striped table-bordered table-responsive  table-hover table-sm"
+                   style="height:500px;display:block;overflow:scroll">
+                <thead >
+                <tr>
+                    <th>First Name</th>
+                    <th>Group</th>
+                    <th>Direction</th>
+                    <th>Learning Status</th>
+                    <th>Employment Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
 
 
-    </div>
-
-    <div class="col-md-2 col-sm-6 ">
-        <div class="row"></div>
-        <label for="learning_status">Статус обучения</label>
-        <div>
-            <select id="learningstatus" class="form-control" name="learning_status">
-                <option value="learning">learning</option>
-                <option value="graduated">graduated</option>
-                <option value="fell_of">fell_of</option>
-                <option value="Other">Other</option>
-            </select>
-        </div>
-        <label for="employment_status">Статус трудоустройства</label>
-        <div>
-            <select id="employmentstatus" name="employment_status" class="form-control">
-                <option value="employed">employed</option>
-                <option value="in_search">in_search</option>
-                <option value="not_relevant_in_IT">not_relevant_in_IT</option>
-                <option value="refused">refused</option>
-                <option value="in_IT_not_in_direction">in_IT_not_in_direction</option>
-                <option value="Other">Other</option>
-            </select>
         </div>
 
-        <button type="button" onclick="window.location='{{ route("addstud")}}'">Добавить нового студента</button>
+        <div class="col-md-2 col-sm-6 ">
+            <div class="row"></div>
+            <label for="learning_status">Статус обучения</label>
+            <div>
+                <select id="learningstatus" class="form-control" name="learning_status">
+                    <OPTION SELECTED VALUE="0" disabled></OPTION>
+                    <option value="learning">learning</option>
+                    <option value="graduated">graduated</option>
+                    <option value="fell_of">fell_of</option>
+                    <option value="Other">Other</option>
+                </select>
+            </div>
+            <label for="employment_status">Статус трудоустройства</label>
+            <div>
+                <select id="employmentstatus" name="employment_status" class="form-control">
+                    <OPTION SELECTED VALUE="0" disabled></OPTION>
+                    <option value="employed">employed</option>
+                    <option value="in_search">in_search</option>
+                    <option value="not_relevant_in_IT">not_relevant_in_IT</option>
+                    <option value="refused">refused</option>
+                    <option value="in_IT_not_in_direction">in_IT_not_in_direction</option>
+                    <option value="Other">Other</option>
+                </select>
+            </div>
+
+            <button type="button" onclick="window.location='{{ route("addstud")}}'">Добавить нового студента</button>
+        </div>
     </div>
 </div>
-
 
 @extends('layouts.footer')
 
