0a92733b3c0c3d8411a3baa83f4a3ac23bf650e1
kib06041960@gnail.com <kib06041960@gnail.com>
2018-11-27 15:43:04 +0200

    alevel is nul comment

--- public/js/companies.js
+++ public/js/companies.js
@@ -20,13 +20,17 @@ function companies(studdata) {
         var cell5 = row.insertCell(4);
         var cell6 = row.insertCell(5);
         var cell7 = row.insertCell(6);
+        let logo;
+        if (!studdata[gr]['logo']) {logo = ''} else {logo = studdata[gr]['logo']}
+        let comment;
+        if (!studdata[gr]['comment']) {comment = ''} else {comment = studdata[gr]['comment']}
         cell1.innerHTML = '<a href="companies/show/' + studdata[gr]['id'] + '">' + studdata[gr]['company_name'] + '</a>';
         cell2.innerHTML = '<a href="companies/show/' + studdata[gr]['id'] + '">' + studdata[gr]['status'] + '</a>';
         cell3.innerHTML = '<a href="companies/show/' + studdata[gr]['id'] + '">' + studdata[gr]['type'] + '</a>';
         cell4.innerHTML = '<a href="companies/show/' + studdata[gr]['id'] + '">' + studdata[gr]['site'] + '</a>';
         cell5.innerHTML = '<a href="companies/show/' + studdata[gr]['id'] + '">' + studdata[gr]['address'] + '</a>';
-        cell6.innerHTML = '<a href="companies/show/' + studdata[gr]['id'] + '">' + studdata[gr]['logo'] + '</a>';
-        cell7.innerHTML = '<a href="companies/show/' + studdata[gr]['id'] + '">' + studdata[gr]['comment'] + '</a>';
+        cell6.innerHTML = '<a href="companies/show/' + studdata[gr]['id'] + '">' + logo + '</a>';
+        cell7.innerHTML = '<a href="companies/show/' + studdata[gr]['id'] + '">' + comment + '</a>';
         rowTable++;
     }
 }
