468a6602d27a602d382c424f7aefde77cf4c3292
kib06041960@gnail.com <kib06041960@gnail.com>
2018-11-28 21:32:48 +0200

    google added

--- resources/views/sms.blade.php
+++ resources/views/sms.blade.php
@@ -3,28 +3,85 @@
 
     <title>Статистика</title>
     <script src="https://www.google.com/jsapi"></script>
-    <script>
-        google.load("visualization", "1", {packages: ["corechart"]});
-        google.setOnLoadCallback(drawChart);
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-        function drawChart() {
-            var data = google.visualization.arrayToDataTable([
-                ['directions', 'quantity'],
-                ['backend', 20],
-                ['frontend', 20],
-                ['fullStack', 40],
-                ['sales', 5],
-                ['PM', 5],
-                ['design', 10],
-            ]);
-            var options = {
-                title: 'На сегодняшний день студентами А-левел являются 100500человек',
-                is3D: true,
-                pieResidueSliceLabel: 'Остальное'
-            };
-            var chart = new google.visualization.PieChart(document.getElementById('circle'));
-            chart.draw(data, options);
+    @csrf
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+
+    <script>
+
+    function getgoogle(){
+            let self = this;
+
+            $.ajax({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                type: "POST",
+                url: location.origin + "/students/studentsalloutput",
+                data: JSON.stringify([1,4,7]),
+                contentType: "application/json",
+                dataType: "json",
+                success: function(dat){
+
+                    let s;
+                    var backend = 0;
+                    var frontend = 0;
+                    var fullStack = 0;
+                    var sales = 0;
+                    var PM = 0;
+                    var design = 0;
+
+                    for(let i=0; i< dat.length; i++ ){
+
+                        if (dat[i]['direction'] =='backend' ) {
+                            backend++;
+                        }
+                        if (dat[i]['direction'] =='frontend' ) {
+                            frontend++;
+                        }
+                        if (dat[i]['direction'] =='fullStack' ) {
+                            fullStack++;
+                        }
+                        if (dat[i]['direction'] =='sales' ) {
+                            sales++;
+                        }
+                        if (dat[i]['direction'] =='PM' ) {
+                            PM++;
+                        }
+                        if (dat[i]['direction'] =='design' ) {
+                            design++;
+                        }
+                    }
+
+                    google.load("visualization", "1", {packages: ["corechart"]});
+                    google.setOnLoadCallback(drawChart);
+
+                    function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                            ['directions', 'quantity'],
+                            ['backend', backend],
+                            ['frontend', frontend],
+                            ['fullStack', fullStack],
+                            ['sales', sales],
+                            ['PM', PM],
+                            ['design', design],
+                        ]);
+                        var options = {
+                            title: 'На сегодняшний день студентами А-левел являются 100500человек',
+                            is3D: true,
+                            pieResidueSliceLabel: 'Остальное'
+                        };
+                        var chart = new google.visualization.PieChart(document.getElementById('circle'));
+                        chart.draw(data, options);
+                    }
+                },
+                failure: function(errMsg) {
+                    //
+                }
+            })
         }
+        getgoogle();
     </script>
 
 
@@ -34,6 +91,8 @@
     <script>
         google.load("visualization", "1", {packages:["corechart"]});
         google.setOnLoadCallback(drawChart);
+
+
         function drawChart() {
             var data = google.visualization.arrayToDataTable([
                 ['employment_status', 'employed', 'in_search', 'not_relevant_in_IT', 'refused','in_IT_not_in_direction','Other'],
