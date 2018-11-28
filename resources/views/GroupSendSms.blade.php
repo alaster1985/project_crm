23b0f5771f43983e331e3caa6c9f26ea1e089a84
IhorBaranchuk <igor.baranchuk333@gmail.com>
2018-11-28 21:17:28 +0200

    Group Send Sms Working

---
+++ resources/views/GroupSendSms.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.nav')
+<h2>Отправка Сообщений </h2>
+<form action="{{Route('sendGroupSms')}}" method="post">
+    @csrf
+    Введите сообщение:<br>
+    <textarea placeholder="Message" name="msg"></textarea><br>
+    <input type="submit" value="Отправить">
+</form>
