9d82a630a66f27d039b279e872ccea05a466134b
Skrypnik Andrii <skrypnik.andrii@gmail.com>
2018-11-29 15:52:55 +0200

    fix_task_validation

--- app/Http/Requests/StoreTask.php
+++ app/Http/Requests/StoreTask.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 
 class StoreTask extends FormRequest
 {
@@ -23,11 +26,20 @@ class StoreTask extends FormRequest
      */
     public function rules()
     {
+        $users_id = DB::table('users')
+            ->get(['id'])
+            ->whereNotIn('id', Auth::id())
+            ->pluck('id')
+            ->toArray();
+
         return [
-            'task_name' => 'required|max:45',
-            'description' => 'required',
-            'dead_line' => 'required|date|after_or_equal:today',
-            'user_id_doer' => 'required',
+            'task_name' => 'required|max:45|min:3',
+            'description' => 'required|max:500|min:5',
+            'dead_line' => 'required|date|after_or_equal:today|before:today + 1 year',
+            'user_id_doer' => [
+                'required',
+                Rule::in($users_id),
+            ],
         ];
     }
 
@@ -37,10 +49,13 @@ class StoreTask extends FormRequest
             'task_name.required' => 'Must be task_name',
             'task_name.max' => 'Shorter please. Max 45',
             'description.required' => 'Must be description',
+            'description.max' => 'Shorter please. Max 500',
             'dead_line.required' => 'Set dead_line, please',
             'dead_line.date' => 'Please, set dead_line as date',
             'dead_line.after_or_equal' => 'Please, set dead_line from today till "Bright future!"',
+            'dead_line.before' => 'Please, set dead_line within one year (365 days)',
             'user_id_doer.required' => 'ChosenOne must be here',
+            'user_id_doer.in' => 'Nice try ;) But set ChosenOne from this select',
         ];
     }
 }
