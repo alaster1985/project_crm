acba1f4fb431debc237d9b98c70355c09104030c
IhorBaranchuk <igor.baranchuk333@gmail.com>
2018-12-03 17:12:57 +0200

    add forms

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -91,14 +91,14 @@
             </table>
         </div>
     </div>
-    <div>
-        <form action="{{Route('rassilka')}}" method="post">
-            @csrf
-            Введите сообщение:<br>
-            <textarea id="msgfield" placeholder="Message" name="msg"></textarea><br>
-            <input id="buttonsend" type="submit" value="Отправить">
-        </form>
-    </div>
+        <div>
+            <form action="{{Route('rassilka')}}" method="post">
+                @csrf
+                Введите сообщение:<br>
+                <textarea id="msgfield" placeholder="Message" name="msg"></textarea><br>
+                <input id="buttonsend" type="submit" value="Отправить">
+            </form>
+        </div>
     <div class="col-md-2 col-sm-2 col-xs-4">
         <div class="row"></div>
         <label for="learning_status">Статус обучения</label>
